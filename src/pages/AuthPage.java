f37a6114ec7fafcb6ec40f20af9da26625eeeb33
Zarko Jovanovic <jovanovic.zarko@outlook.com>
2021-02-23 17:41:12 +0100

    Push Update AuthPage

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AuthPage extends BasicPage{
@@ -10,4 +12,16 @@ public class AuthPage extends BasicPage{
 		super(driver, waiter, js);
 	}
 
+	public WebElement getProfileName() {
+		return this.driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div[2]/div[2]/ul/li/a"));
+}
+	
+	public WebElement getLogOutBtn() {
+		return this.driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div[2]/div[2]/ul/li/div/ul/li[2]/a"));
+	}
+	
+	public void LogOutAccount() {
+		this.getProfileName().click();
+		this.getLogOutBtn().click();
+	}
 }
